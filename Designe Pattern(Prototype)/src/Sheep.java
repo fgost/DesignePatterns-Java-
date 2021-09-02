522c223672f61adaaca487e56c6b1b98baec06a7
Matheus Firmiano <56655021+fgost@users.noreply.github.com>
2021-09-02 13:56:46 -0300

    Add files via upload

---
+++ Designe Pattern(Prototype)/src/Sheep.java
@@ -0,0 +1,20 @@
+public class Sheep implements Animal{
+    public Sheep(){
+        System.out.println("Sheep is made");
+    }
+    @Override
+    public Animal makeCopy() {
+        System.out.println("Sheep is Being Made");
+        Sheep sheepObject = null;
+        try {
+            sheepObject = (Sheep) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return sheepObject;
+    }
+    public String toString(){
+        return "Coke is my Hero, Baaaaa";
+    }
+}
